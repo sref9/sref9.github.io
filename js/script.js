3ea2f8bb8539b8aeda3aac69648ee0d01715a4fb
sref9 <121738415+sref9@users.noreply.github.com>
2023-01-02 14:51:31 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -206,7 +206,7 @@ class NoiseOsc {
     this.ampl = ampl;
     this.env = new p5.Env();
     this.env.setADSR(2, 1, 1,2);
-    this.env.setRange(0,1);
+    this.env.setRange(0,0);
     this.osc.amp(this.env);
 
 
