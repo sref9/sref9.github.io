871000c1a063175456b5719f4b5853f86d1e5dc7
sref9 <121738415+sref9@users.noreply.github.com>
2023-01-02 14:32:42 -0600

    02/01/23 (#1)
    * Delete Infinity.ttf
    
    * Delete js directory
    
    * Delete css directory
    
    * Add files via upload
    
    * Update index.html
    
    * Update script.js

--- js/script.js
+++ js/script.js
@@ -205,7 +205,7 @@ class NoiseOsc {
     this.osc = new p5.Noise();
     this.ampl = ampl;
     this.env = new p5.Env();
-    this.env.setADSR(2, 0.3, 0.3,2);
+    this.env.setADSR(2, 0.3, 1,2);
     this.env.setRange(0.7,0);
     this.osc.amp(this.env);
 
